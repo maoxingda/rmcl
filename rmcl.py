4578031598a5e46ebfb0fe6b8ea687e27ff32b8f
maoxd <maoxingda@meican.com>
2022-07-23 22:53:54 +0800

    实现tpw-wallet报表模板参数替换以及还原

--- rmcl.py
+++ rmcl.py
@@ -49,5 +49,45 @@ def a7(
             f.write(sql)
 
 
+@main.command()
+@click.option('-r', '--render/--no-render', default=False)
+@click.option('-c', '--client-snowflake-id', required=True, prompt='客户雪花ID')
+@click.option('-a', '--wallet-id', required=True, prompt='第三方钱包账户ID')
+@click.option('-s', '--start-date', required=True, prompt='报表查询开始日期')
+@click.option('-e', '--end-date', required=True, prompt='报表查询截止日期')
+@click.argument('file_name', required=True)
+def t1(
+        render,
+        client_snowflake_id,
+        wallet_id,
+        start_date,
+        end_date,
+        file_name,
+):
+    work_dir = os.getcwd()
+    sql_file_path = f'{work_dir}/{file_name}'
+
+    if render:
+        with open(sql_file_path) as f:
+            sql = Template(f.read()).render({
+                'client_snowflake_id': client_snowflake_id,
+                'wallet_id': wallet_id,
+                'date_start': start_date,
+                'date_end': end_date,
+            })
+
+        with open(sql_file_path, 'w') as f:
+            f.write(sql)
+    else:
+        with open(sql_file_path) as f:
+            sql = f.read().replace(client_snowflake_id, '{{client_snowflake_id}}')
+            sql = sql.replace(wallet_id, '{{wallet_id}}')
+            sql = sql.replace(start_date, '{{date_start}}')
+            sql = sql.replace(end_date, '{{date_end}}')
+
+        with open(sql_file_path, 'w') as f:
+            f.write(sql)
+
+
 if __name__ == '__main__':
     main()
