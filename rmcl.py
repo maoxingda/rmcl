39d0b2c894b440b85c68b8af614a277ffe0f1d61
maoxd <maoxingda@meican.com>
2022-07-26 17:21:28 +0800

    实现S6报表模板参数替换以及还原

--- rmcl.py
+++ rmcl.py
@@ -89,5 +89,41 @@ def t1(
             f.write(sql)
 
 
+@main.command()
+@click.option('-r', '--render/--no-render', default=False)
+@click.option('-s', '--subsidy-snowflake-id', required=True, prompt='餐补雪花ID')
+@click.option('-s', '--start-date', required=True, prompt='报表查询开始日期')
+@click.option('-e', '--end-date', required=True, prompt='报表查询截止日期')
+@click.argument('file_name', required=True, type=click.Path(exists=True))
+def s6(
+        render,
+        subsidy_snowflake_id,
+        start_date,
+        end_date,
+        file_name,
+):
+    work_dir = os.getcwd()
+    sql_file_path = f'{work_dir}/{file_name}'
+
+    if render:
+        with open(sql_file_path) as f:
+            sql = Template(f.read()).render({
+                'subsidy_snowflake_id': subsidy_snowflake_id,
+                'date_start': start_date,
+                'date_end': end_date,
+            })
+
+        with open(sql_file_path, 'w') as f:
+            f.write(sql)
+    else:
+        with open(sql_file_path) as f:
+            sql = f.read().replace(subsidy_snowflake_id, '{{subsidy_snowflake_id}}')
+            sql = sql.replace(start_date, '{{date_start}}')
+            sql = sql.replace(end_date, '{{date_end}}')
+
+        with open(sql_file_path, 'w') as f:
+            f.write(sql)
+
+
 if __name__ == '__main__':
     main()
