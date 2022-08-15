ee79d577e77c58ad65d8cea5d5d9f12118f49d87
maoxd <maoxingda@meican.com>
2022-08-15 16:22:14 +0800

    实现R15报表模板参数替换以及还原

--- rmcl.py
+++ rmcl.py
@@ -125,5 +125,41 @@ def s6(
             f.write(sql)
 
 
+@main.command()
+@click.option('-r', '--render/--no-render', default=False)
+@click.option('-s', '--client-member-sn-id', required=True, prompt='客户成员雪花ID')
+@click.option('-s', '--start-date', required=True, prompt='报表查询开始日期')
+@click.option('-e', '--end-date', required=True, prompt='报表查询截止日期')
+@click.argument('file_name', required=True, type=click.Path(exists=True))
+def s6(
+        render,
+        client_member_sn_id,
+        start_date,
+        end_date,
+        file_name,
+):
+    work_dir = os.getcwd()
+    sql_file_path = f'{work_dir}/{file_name}'
+
+    if render:
+        with open(sql_file_path) as f:
+            sql = Template(f.read()).render({
+                'client_member_sn_id': client_member_sn_id,
+                'date_start': start_date,
+                'date_end': end_date,
+            })
+
+        with open(sql_file_path, 'w') as f:
+            f.write(sql)
+    else:
+        with open(sql_file_path) as f:
+            sql = f.read().replace(client_member_sn_id, '{{client_member_sn_id}}')
+            sql = sql.replace(start_date, '{{date_start}}')
+            sql = sql.replace(end_date, '{{date_end}}')
+
+        with open(sql_file_path, 'w') as f:
+            f.write(sql)
+
+
 if __name__ == '__main__':
     main()
